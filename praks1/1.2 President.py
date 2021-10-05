8e682a5876eabc3f33fc028c3061502904a55e7c
Danil Kersontsev <danil.kersontsev@khk.ee>
2021-10-05 13:37:32 +0300

    1.2 president

---
+++ praks1/1.2 President.py
@@ -0,0 +1,8 @@
+# defineerin muutuja ja omistan väärtused
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause_keskosa = ". aastal valiti Eesti presidendiks "
+
+# muutujate kasutamine
+Lause = str(aasta) + lause_keskosa + president
+print(Lause)
