392d35b5eb6b4b53a1b01e33b837c5f750f441b3
Danil Kersontsev <danil.kersontsev@khk.ee>
2021-10-23 17:16:36 +0300

    2.2 Pulss

---
+++ praks1/2.2 Pulss.py
@@ -0,0 +1,19 @@
+vanus = int(input("Sisestage enda vanus: "))
+sugu = input("Sisestage enda sugu: ").upper()
+treeningtüüp = int(input("Sisestage treeningu tüüp: "))
+
+if (sugu == "M"):
+    tervisetreening = 220 - vanus
+if (sugu == "N"):
+    tervisetreening = 206 - (vanus * 0.88)
+
+if treeningtüüp == 1:
+    tervisetreening_min = round(tervisetreening * 0.5)
+    tervisetreening_max = round(tervisetreening * 0.7)
+if treeningtüüp == 2:
+    tervisetreening_min = round(tervisetreening * 0.7)
+    tervisetreening_max = round(tervisetreening * 0.8)
+if treeningtüüp == 3:
+    tervisetreening_min = round(tervisetreening * 0.8)
+    tervisetreening_max = round(tervisetreening * 0.87)
+print("Pulsisagedus peaks olema vahemikus " + str(tervisetreening_min) + " kuni " + str(tervisetreening_max) + ".")
